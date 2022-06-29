afd118ef7c59acf37398dc5a0906d83cad323a7f
saidugurlu <ugurlumsaid@gmx.de>
2022-06-29 13:50:21 +0200

    form is in

--- src/App.jsx
+++ src/App.jsx
@@ -3,25 +3,49 @@ import axios from 'axios';
 import './App.scss';
 import Modal from './components/Modal';
 const url = 'http://localhost:3042/flashcards';
-
 function App() {
     const [flashcards, setFlashcards] = useState([]);
     const [fieldCategory, setFieldCategory] = useState('');
+    const [fieldFront, setFieldFront] = useState('');
+    const [fieldBack, setFieldBack] = useState('');
 
     useEffect(() => {
         (async () => {
             setFlashcards((await axios.get(url)).data);
         })();
     }, []);
+
     return (
         <div className="App">
-            <h1>Flashcards</h1>
-            [{fieldCategory}]
+            <h1>Flashcards</h1>[{fieldCategory}]
             <p>There are {flashcards.length} flashcards.</p>
             <Modal buttonText="Add New Flashcard">
                 <h2>Add Flashcard</h2>
                 <form className="modalContent">
-                    Category: <input value={fieldCategory} onChange={(e) => setFieldCategory(e.target.value)}/> 
+                    <div className="row">
+                        Category:{' '}
+                        <input
+                            value={fieldCategory}
+                            onChange={(e) => setFieldCategory(e.target.value)}
+                        />
+                    </div>
+                    <div className="row">
+                        Front:{' '}
+                        <input
+                            value={fieldFront}
+                            onChange={(e) => setFieldFront(e.target.value)}
+                        />
+                    </div>
+                    <div className="row">
+                        Back:{' '}
+                        <input
+                            value={fieldBack}
+                            onChange={(e) => setFieldBack(e.target.value)}
+                        />
+                    </div>
+					<div className="row">
+						<button className="saveFlashcard">Save</button>
+						</div>
                 </form>
             </Modal>
         </div>

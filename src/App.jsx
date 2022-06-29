76361555944eb75e7807132bc6a205ccbd322893
saidugurlu <ugurlumsaid@gmx.de>
2022-06-29 13:25:55 +0200

    backend connected and work

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,24 @@
-import './App.scss'
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+import './App.scss';
+
+const url = 'http://localhost:3042/flashcards';
 
 function App() {
-  return (
-    <div className="App">
-      <h1>Flashcards</h1>
-    </div>
-  )
+    const [flashcards, setFlashcards] = useState([]);
+
+    useEffect(() => {
+        (async () => {
+            setFlashcards((await axios.get(url)).data);
+        })();
+    }, []);
+
+    return (
+        <div className="App">
+            <h1>Flashcards</h1>
+            <p>There are {flashcards.length} flashcards.</p>
+        </div>
+    );
 }
 
-export default App
+export default App;
